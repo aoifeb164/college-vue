97e2cce50c84f0c7539e0caeb4a07ca6de662577
aoifeb164 <n00182228@student.iadt.ie>
2021-04-19 16:57:16 +0100

    hosting

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,11 @@
 /**
  * @Date:   2021-03-16T09:15:36+00:00
- * @Last modified time: 2021-03-16T09:22:40+00:00
+ * @Last modified time: 2021-04-19T16:33:45+01:00
  */
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://college.api:8000/api'
+  baseURL: 'https://college-api-aoife.herokuapp.com/api'
 })
 
 
